a287f8b36d821a49c379c056eeb547ac04f149a7
ut61z <ytytytytytyt1@gmail.com>
2022-12-03 18:44:31 +0900

    🎉 fisrt commit

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+import type { SocialObjects } from "./types";
+
+export const SITE = {
+  website: "https://astro-paper.pages.dev/",
+  author: "Yuta Endo",
+  desc: "ut61z's blog",
+  title: "ut61z's blog",
+  ogImage: "default-og.png",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/ut61z",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/ut61z",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+];
